057674915a48380fadca16ea03a74f72c705a43f
oleksandr.riznyk <oleksandr.riznyk.k@gmail.com>
2021-08-15 10:17:49 +0300

    Initial commit

---
+++ src/test/java/com/smirnov/helloworldaws/HelloWorldAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smirnov.helloworldaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
